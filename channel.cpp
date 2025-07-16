737d42373540a0ea723b53eb21bf936f4e63ba8b
HuanXu <87217406+HuannnXu@users.noreply.github.com>
2025-07-15 21:32:28 -0700

    Add files via upload

---
+++ channel.cpp
@@ -0,0 +1,11 @@
+#include "channel.h"
+#include <vector>
+#include <complex>
+using namespace std;
+
+channel::channel(trxConfig& config){
+
+}
+vector<vector<complex<double> > > channel::fadingChannel(vector<vector<complex<double> > >& txsignal){
+
+}
